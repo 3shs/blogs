3d151ffd09f386eafe680bd2f95737dbd9f6fa38
3shs <875755045@qq.com>
2022-07-21 17:32:19 +0800

    remove some

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,14 +1,25 @@
 module.exports = {
+  title: '一些零碎 | 想起就记录一下',
+  head: [
+    ['link', { rel: 'icon', href: '/logo.png' }]
+  ],
   themeConfig: {
     search: false,
+    editLinks: true,
     searchMaxSuggestions: 10,
     sidebar: [
       {
         title: 'Vue相关知识',
         collapsable: false,
         children: [
-          ['vue/', '综述'],
           ['complie/', '模板解析']
+        ]
+      },
+      {
+        title: 'Webpack相关知识',
+        collapsable: false,
+        children: [
+          
         ]
       }
     ]
