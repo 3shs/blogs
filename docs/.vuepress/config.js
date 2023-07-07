b5bd6b0ce97edc4bdb8e330f2f11aa40c034dba6
linzy <875755045@qq.com>
2023-07-07 14:48:50 +0800

    fix directory structure

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -15,26 +15,26 @@ module.exports = {
         title: 'Vue相关知识',
         collapsable: false,
         children: [
-          ['complie/', '模板解析'],
-          ['genRender/', '生成render函数'],
-          ['render/', '挂载渲染阶段'],
-          ['update/', '更新阶段'],
-          ['component/', '组件分析'],
+          ['vue/complie/', '模板解析'],
+          ['vue/genRender/', '生成render函数'],
+          ['vue/render/', '挂载渲染阶段'],
+          ['vue/update/', '更新阶段'],
+          ['vue/component/', '组件分析'],
         ]
       },
       {
         title: 'Webpack相关知识',
         collapsable: false,
         children: [
-
+          ['webpack/optimization/', '优化'],
         ]
       },
       {
         title: '性能相关',
         collapsable: false,
         children: [
-          ['virtualScroll/', '虚拟滚动'],
-          ['cacheApi/', '接口缓存']
+          ['performance/virtualScroll/', '虚拟滚动'],
+          ['performance/cacheApi/', '接口缓存']
         ]
       }
     ]
